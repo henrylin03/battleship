98f69e141a6c5b527b3fc95d6e6708664c583d81
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-08-09 11:40:04 +1000

    fix: test: ensure ship unit tests around whether .isSunk() is returning true or false is specifically true or false expected, and not truthy or falsy - the latter including undefined and null data types which would not be correct

--- src/tests/createShip.test.js
+++ src/tests/createShip.test.js
@@ -3,12 +3,12 @@ import createShip from "../js/createShip";
 test("a carrier (ship with length=5) is not sunk after being hit once", () => {
   const carrier = createShip(5);
   carrier.hit();
-  expect(carrier.isSunk()).toBeFalsy();
+  expect(carrier.isSunk()).toBe(false);
 });
 
 test("a destroyer (ship with length=2) is sunk after being hit twice", () => {
   const destroyer = createShip(2);
   destroyer.hit();
   destroyer.hit();
-  expect(destroyer.isSunk()).toBeTruthy();
+  expect(destroyer.isSunk()).toBe(true);
 });
