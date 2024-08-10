8863eb3f63b01f2eaed38534d7ed347f68d4da9b
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-08-10 14:10:20 +1000

    setup basic html and css for everything from top down on page up until the grids

--- src/js/createScreenController.js
+++ src/js/createScreenController.js
@@ -1,14 +1,15 @@
 import createGameController from "./createGameController";
 
+const createGrid = () => {};
+
 const createScreenController = () => {
   const game = createGameController();
 
   // method: update the screen after every turn
   const updateScreen = () => {
     // if opponent is computer, then their screen is always hidden, other than white pins (misses) vs red pins (hit)
-
     // on your own screen, you can see your ships' locations once game begins
-  }
+  };
 
   return;
 };
