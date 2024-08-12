41eb0081b2cab8a24b515c800ba13c14dd8a8f67
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-08-12 10:04:46 +1000

    style: position grids and overall page using padding, dynamic width of the container for the grids

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import "./assets/styles/normalise.css";
 import "./assets/styles/style.css";
+import createScreenController from "./js/createScreenController";
+
+createScreenController();
--- src/js/createScreenController.js
+++ src/js/createScreenController.js
@@ -1,6 +1,33 @@
 import createGameController from "./createGameController";
 
-const createGrid = () => {};
+const createGrids = () => {
+  const SQUARES_PER_SIDE = 10;
+  const grids = document.querySelectorAll(".grid");
+
+  Array.from(grids).forEach((container) => {
+    // delete existing grid, if any
+    container.replaceChildren();
+
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+
+    let squaresCount = 1;
+    while (squaresCount <= SQUARES_PER_SIDE) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+      squaresCount++;
+    }
+
+    let rowCount = 1;
+    while (rowCount + 1 <= SQUARES_PER_SIDE) {
+      const rowClone = row.cloneNode(true);
+      container.appendChild(rowClone);
+      rowCount++;
+    }
+  });
+};
 
 const createScreenController = () => {
   const game = createGameController();
@@ -9,9 +36,10 @@ const createScreenController = () => {
   const updateScreen = () => {
     // if opponent is computer, then their screen is always hidden, other than white pins (misses) vs red pins (hit)
     // on your own screen, you can see your ships' locations once game begins
+    return;
   };
 
-  return;
+  createGrids();
 };
 
 export default createScreenController;
