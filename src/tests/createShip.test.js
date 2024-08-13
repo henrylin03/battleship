af6b02bfb273ba38b7f14871c14cd202bbfc8f77
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-08-13 18:11:49 +1000

    test(createShip factory): remove tests for setting coordinates, as these are now being done in the gameboard factory as of 81f703348c01d86eec2d5f9b3ab32eb7ab70e40f

--- src/tests/createShip.test.js
+++ src/tests/createShip.test.js
@@ -14,25 +14,3 @@ describe("Check ship-sinking behaviour", () => {
     expect(patrolShip.isSunk()).toBe(true);
   });
 });
-
-describe("Check ship's coordinates are properly set", () => {
-  test("A destroyer (length=3), starting at [0, 0] and is horizontal, has coordinates: [ [0, 0], [1, 0], [2, 0] ]", () => {
-    const destroyer = createShip(3);
-    destroyer.setCoordinates([0, 0], true);
-    expect(destroyer.getCoordinates()).toEqual([
-      [0, 0],
-      [1, 0],
-      [2, 0],
-    ]);
-  });
-
-  test("A destroyer (length=3), starting at [9, 9] and is vertical, has coordinates: [[9,9], [9,8], [9,7]]", () => {
-    const destroyer = createShip(3);
-    destroyer.setCoordinates([9, 9], false);
-    expect(destroyer.getCoordinates()).toEqual([
-      [9, 9],
-      [9, 8],
-      [9, 7],
-    ]);
-  });
-});
